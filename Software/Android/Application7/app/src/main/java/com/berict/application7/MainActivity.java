eba23c0e5e960bd710796137bdc9bd2444a0534a
BedrockSunrin <dev.bedrock@gmail.com>
2018-03-22 10:32:18 +0900

    Added layout inflating examples

--- Software/Android/Application7/app/src/main/java/com/berict/application7/MainActivity.java
+++ Software/Android/Application7/app/src/main/java/com/berict/application7/MainActivity.java
@@ -1,193 +1,60 @@
 package com.berict.application7;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText username;
-    EditText name;
-    EditText address;
-
-    RadioGroup age;
-    String ageGroup;
-
-    TextView usernameText;
-    TextView nameText;
-    TextView addressText;
-    TextView ageText;
-    TextView termsText;
-
-    TextView usernameResult;
-    TextView nameResult;
-    TextView addressResult;
-    TextView ageResult;
-
-    View result;
-
-    CheckBox terms;
-    boolean isTermAgree = false;
+    LinearLayout sub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // an alternative way to do this:
+        //setContentView(R.layout.activity_main);
+
+//        LinearLayout linear = new LinearLayout(this);
+//        linear.setOrientation(LinearLayout.VERTICAL);
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT
+//        );
+//
+//        TextView text = new TextView(this);
+//        text.setText("Hello World!!");
+//        text.setTextSize(24);
+//        text.setTextColor(Color.BLUE);
+//
+//        linear.addView(text);
+//
+//        setContentView(linear);
+
+        // inflate a custom layout
         setContentView(R.layout.activity_main);
 
-        username = findViewById(R.id.edittext_username);
-        name = findViewById(R.id.edittext_name);
-        address = findViewById(R.id.edittext_address);
-        age = findViewById(R.id.radiogroup_age);
-        terms = findViewById(R.id.checkbox_terms);
+        sub = findViewById(R.id.sub);
 
-        usernameText = findViewById(R.id.text_username);
-        nameText = findViewById(R.id.text_name);
-        addressText = findViewById(R.id.text_address);
-        ageText = findViewById(R.id.text_age);
-        termsText = findViewById(R.id.text_terms);
-
-        usernameResult = findViewById(R.id.result_username);
-        nameResult = findViewById(R.id.result_name);
-        addressResult = findViewById(R.id.result_address);
-        ageResult = findViewById(R.id.result_age);
-
-        result = findViewById(R.id.result);
-
-        username.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                int length = editable.toString().length();
-                if (length > 8 || length == 0) {
-                    username.setTextColor(Color.RED);
-                    usernameText.setTextColor(Color.RED);
-                } else {
-                    username.setTextColor(Color.BLACK);
-                    usernameText.setTextColor(Color.BLACK);
-                }
-            }
-        });
-
-        name.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                int length = editable.toString().length();
-                if (length == 0) {
-                    nameText.setTextColor(Color.RED);
-                } else {
-                    nameText.setTextColor(Color.BLACK);
-                }
-            }
-        });
-
-        address.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                int length = editable.toString().length();
-                if (length == 0) {
-                    addressText.setTextColor(Color.RED);
-                } else {
-                    addressText.setTextColor(Color.BLACK);
-                }
-            }
-        });
-
-        age.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                ageGroup = ((RadioButton) radioGroup.getChildAt(i - 1)).getText().toString();
-                ageText.setTextColor(Color.BLACK);
-            }
-        });
-
-        findViewById(R.id.layout_terms).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                terms.toggle();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub, sub, true);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // also can access the views in sub layout
+                        ((TextView) findViewById(R.id.sub_text)).setText("Loaded");
+                    }
+                }, 1000);
             }
         });
-
-        terms.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                isTermAgree = b;
-                if (isTermAgree == true) {
-                    termsText.setTextColor(Color.BLACK);
-                }
-            }
-        });
-    }
-
-    public void save(View v) {
-        boolean isFilled = true;
-
-        if (username.getText().toString().length() == 0 || username.getText().toString().length() > 8) {
-            usernameText.setTextColor(Color.RED);
-            isFilled = false;
-        }
-
-        if (name.getText().toString().length() == 0) {
-            nameText.setTextColor(Color.RED);
-            isFilled = false;
-        }
-
-        if (address.getText().toString().length() == 0) {
-            addressText.setTextColor(Color.RED);
-            isFilled = false;
-        }
-
-        if (age.getCheckedRadioButtonId() < 0) {
-            ageText.setTextColor(Color.RED);
-            isFilled = false;
-        }
-
-        if (terms.isChecked() == false) {
-            termsText.setTextColor(Color.RED);
-            isFilled = false;
-        }
-
-        if (isFilled) {
-            result.setVisibility(View.VISIBLE);
-            usernameResult.setText("Username : " + username.getText().toString());
-            nameResult.setText("Name : " + name.getText().toString());
-            addressResult.setText("Address : " + address.getText().toString());
-            ageResult.setText("Age : " + ageGroup);
-        }
-    }
-
-    public void close(View v) {
-        finish();
     }
 }
---
+++ Software/Android/Application7/app/src/main/java/com/berict/application7/Problem5Activity.java
@@ -0,0 +1,193 @@
+package com.berict.application7;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class Problem5Activity extends AppCompatActivity {
+
+    EditText username;
+    EditText name;
+    EditText address;
+
+    RadioGroup age;
+    String ageGroup;
+
+    TextView usernameText;
+    TextView nameText;
+    TextView addressText;
+    TextView ageText;
+    TextView termsText;
+
+    TextView usernameResult;
+    TextView nameResult;
+    TextView addressResult;
+    TextView ageResult;
+
+    View result;
+
+    CheckBox terms;
+    boolean isTermAgree = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.problem5);
+
+        username = findViewById(R.id.edittext_username);
+        name = findViewById(R.id.edittext_name);
+        address = findViewById(R.id.edittext_address);
+        age = findViewById(R.id.radiogroup_age);
+        terms = findViewById(R.id.checkbox_terms);
+
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        addressText = findViewById(R.id.text_address);
+        ageText = findViewById(R.id.text_age);
+        termsText = findViewById(R.id.text_terms);
+
+        usernameResult = findViewById(R.id.result_username);
+        nameResult = findViewById(R.id.result_name);
+        addressResult = findViewById(R.id.result_address);
+        ageResult = findViewById(R.id.result_age);
+
+        result = findViewById(R.id.result);
+
+        username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int length = editable.toString().length();
+                if (length > 8 || length == 0) {
+                    username.setTextColor(Color.RED);
+                    usernameText.setTextColor(Color.RED);
+                } else {
+                    username.setTextColor(Color.BLACK);
+                    usernameText.setTextColor(Color.BLACK);
+                }
+            }
+        });
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int length = editable.toString().length();
+                if (length == 0) {
+                    nameText.setTextColor(Color.RED);
+                } else {
+                    nameText.setTextColor(Color.BLACK);
+                }
+            }
+        });
+
+        address.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int length = editable.toString().length();
+                if (length == 0) {
+                    addressText.setTextColor(Color.RED);
+                } else {
+                    addressText.setTextColor(Color.BLACK);
+                }
+            }
+        });
+
+        age.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                ageGroup = ((RadioButton) radioGroup.getChildAt(i - 1)).getText().toString();
+                ageText.setTextColor(Color.BLACK);
+            }
+        });
+
+        findViewById(R.id.layout_terms).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                terms.toggle();
+            }
+        });
+
+        terms.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                isTermAgree = b;
+                if (isTermAgree == true) {
+                    termsText.setTextColor(Color.BLACK);
+                }
+            }
+        });
+    }
+
+    public void save(View v) {
+        boolean isFilled = true;
+
+        if (username.getText().toString().length() == 0 || username.getText().toString().length() > 8) {
+            usernameText.setTextColor(Color.RED);
+            isFilled = false;
+        }
+
+        if (name.getText().toString().length() == 0) {
+            nameText.setTextColor(Color.RED);
+            isFilled = false;
+        }
+
+        if (address.getText().toString().length() == 0) {
+            addressText.setTextColor(Color.RED);
+            isFilled = false;
+        }
+
+        if (age.getCheckedRadioButtonId() < 0) {
+            ageText.setTextColor(Color.RED);
+            isFilled = false;
+        }
+
+        if (terms.isChecked() == false) {
+            termsText.setTextColor(Color.RED);
+            isFilled = false;
+        }
+
+        if (isFilled) {
+            result.setVisibility(View.VISIBLE);
+            usernameResult.setText("Username : " + username.getText().toString());
+            nameResult.setText("Name : " + name.getText().toString());
+            addressResult.setText("Address : " + address.getText().toString());
+            ageResult.setText("Age : " + ageGroup);
+        }
+    }
+
+    public void close(View v) {
+        finish();
+    }
+}
