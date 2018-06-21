a82d41fec48adb140098f118ebbbc4f5db6459f4
BedrockSunrin <dev.bedrock@gmail.com>
2018-06-21 09:17:58 +0900

    Added Android intent examples (final)

--- Software/Android/Application24/app/src/main/java/com/berict/application24/MainActivity.java
+++ Software/Android/Application24/app/src/main/java/com/berict/application24/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
@@ -21,6 +22,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -119,35 +121,48 @@ public class MainActivity extends AppCompatActivity {
         speechBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition");
+                startActivityForResult(intent, 50);
             }
         });
 
         mapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.5662952, 126.9779451?q=37.5662952, 126.9779451"));
+                startActivity(intent);
             }
         });
 
         browserBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://berict.com"));
+                startActivity(intent);
             }
         });
 
         callBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (ContextCompat.checkSelfPermission(activity, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:02-120"));
+                    startActivity(intent);
+                } else {
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CALL_PHONE}, 100);
+                }
             }
         });
 
         galleryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 40);
             }
         });
 
@@ -159,23 +174,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == RESULT_OK) {
-            switch (requestCode) {
-                case 10:
-                    resultView.setText(data.getDataString());
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse(ContactsContract.Contacts.CONTENT_URI + "/" + 1));
-                    startActivity(intent);
-                    break;
-                case 20:
-                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-                    resultImageView.setImageBitmap(bitmap);
-                    break;
-                case 30:
-                    if (filePath != null) {
-                        Bitmap bitmapFile = BitmapFactory.decodeFile(filePath.getAbsolutePath());
-                        resultImageView.setImageBitmap(bitmapFile);
-                    }
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 10) {
+                resultView.setText(data.getDataString());
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(ContactsContract.Contacts.CONTENT_URI + "/" + 1));
+                startActivity(intent);
+
+            } else if (requestCode == 20) {
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                resultImageView.setImageBitmap(bitmap);
+
+            } else if (requestCode == 30) {
+                if (filePath != null) {
+                    Bitmap bitmapFile = BitmapFactory.decodeFile(filePath.getAbsolutePath());
+                    resultImageView.setImageBitmap(bitmapFile);
+                }
+            } else if (requestCode == 40) {
+                String result = data.getDataString();
+                resultView.setText(result);
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                Uri photoURI = FileProvider.getUriForFile(
+                        MainActivity.this,
+                        BuildConfig.APPLICATION_ID + ".provider",
+                        filePath
+                );
+                intent.setDataAndType(photoURI, "image/");
+                intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+                startActivity(intent);
+            } else if (requestCode == 50) {
+                ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                String result = results.get(0);
+
+                resultView.setText(result);
             }
         }
     }
